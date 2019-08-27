457478e334f4ee71de128fa5663f9d7f2831bb17
yassinechemli <yassine7b8@gmail.com>
2019-08-27 17:04:18 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import "./style.css";
 import './App.css';
+import image1 from  "./image1.png";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+    <div >
+	<h1 className="title red">My title</h1>
+
+	<br/>
+
+<img src={image1} />
+<br/>
+	<img src="/image2.jpg" />
+</div>
 export default App;
